773d2789d16f2bea219b2d0d627e6de89fd219cb
Firas12123 <firascosta35@gmail.com>
2026-08-26 14:18:36 +0100

    Executes file sorting and creates folder inside given directory without functions

--- file_organiser/main.py
+++ file_organiser/main.py
@@ -1,1 +1,28 @@
-# My Smart File Organiser Project
+import os
+from pathlib import Path
+import shutil
+import hashlib
+#create a dictionary with the file types with a list of the file extensions needed
+
+file_types = {"Images": [".png",".jpg",".jpeg", ".gif"],
+              "Videos": [".mov",".mp4",".m4v", ".qt",".webm",".wmv",".avi",".flv",".f4v"],
+              "Documents": [".pdf", ".docx",".txt",".csv",".doc",".rtf"],
+              "Code": [".py",".html",".css",".js"]}
+ 
+target_dir = Path.home() / "Documents" # find the correct files
+for file in target_dir.iterdir():
+    if file.is_file():
+        file_ext = file.suffix.lower()
+        for key, values in file_types.items():
+            for ext in values:
+                if file_ext == ext:
+                    new_path = target_dir / key
+                    if not os.path.exists(new_path):
+                        os.makedirs(new_path)
+                    shutil.move(file, new_path)
+                    
+                    
+                
+                
+
+    
